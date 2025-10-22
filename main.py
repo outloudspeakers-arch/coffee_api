8f2ce6f177cfd95918da8dbfbd26bb58071a2288
outloudspeakers-arch <outloudspeakers@gmail.com>
2025-10-22 18:22:34 +0500

    Initial commit

--- main.py
+++ main.py
@@ -168,3 +168,7 @@ def about():
         ],
         "quote": "Learn English, one sip at a time ☕📖"
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
